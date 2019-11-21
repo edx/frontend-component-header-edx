9935167713b8599ee04d41051d8d890662b707c0
Adam Butterworth <abutterworth@edx.org>
2019-11-21 17:12:52 -0500

    refactor: update getLearnerPortalLinks

--- src/enterprise/getLearnerPortalLinks.js
+++ src/enterprise/getLearnerPortalLinks.js
@@ -1,69 +1,54 @@
 import { getAuthenticatedUser, getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 import { getConfig } from '@edx/frontend-platform/config';
 
-const fetchEnterpriseCustomers = () => getAuthenticatedHttpClient()
-  .get(`${getConfig().LMS_BASE_URL}/enterprise/api/v1/enterprise-customer/`);
+const isEnterpriseLearner = (user = {}) => {
+  const { roles = [] } = user;
+  return roles.some(role => role.includes('enterprise_learner'));
+};
 
+const getCacheKey = userId => `learnerPortalLinks:${userId}`;
 
-function isEnterpriseLearner(user) {
-  if (user !== null && user.roles) {
-    const { roles } = user;
-    for (let i = 0; i < roles.length; i += 1) {
-      if (roles[i].includes('enterprise_learner')) {
-        return true;
-      }
-    }
-  }
-
-  return false;
-}
-
-function getCacheKey(userId) {
-  return `learnerPortalLinks:${userId}`;
-}
-
-function cacheLinks(userId, links) {
-  sessionStorage.setItem(
-    getCacheKey(userId),
-    JSON.stringify({
-      // Set one hour expiration
-      expiration: Date.now() + (1 * 60 * 60 * 1000),
-      links,
-    }),
-  );
-}
-
-async function fetchLearnerPortalLinks(apiClient, userId) {
-  const learnerPortalLinks = [];
-  const response = await fetchEnterpriseCustomers(apiClient);
-  const enterpriseCustomers = response.data.results;
-  try {
-    for (let i = 0; i < enterpriseCustomers.length; i += 1) {
-      const enterpriseCustomer = enterpriseCustomers[i];
-      const enableLearnerPortal = enterpriseCustomer.enable_learner_portal;
-      const learnerPortalHostname = enterpriseCustomer.learner_portal_hostname;
-      if (enableLearnerPortal && learnerPortalHostname) {
-        learnerPortalLinks.push({
-          title: `${enterpriseCustomer.name}`,
-          url: `https://${enterpriseCustomer.learner_portal_hostname}`,
-        });
-      }
-    }
-
-    cacheLinks(userId, learnerPortalLinks);
-  } catch (error) {
-    // empty
-  }
-
-  return learnerPortalLinks;
-}
-
-function getCachedLearnerPortalLinks(userId) {
+const cacheLinks = (userId, links) => {
+  // Set one hour expiration
+  const expiration = Date.now() + (1 * 60 * 60 * 1000);
   const cacheKey = getCacheKey(userId);
-  const cachedItem = sessionStorage.getItem(cacheKey);
+  const cacheValue = JSON.stringify({ expiration, links });
+  sessionStorage.setItem(cacheKey, cacheValue);
+};
 
-  if (cachedItem) {
-    const cachedLinks = JSON.parse(cachedItem);
+const fetchLearnerPortalLinks = async (userId) => {
+  const httpClient = getAuthenticatedHttpClient();
+  const enterpriseApiUrl = `${getConfig().LMS_BASE_URL}/enterprise/api/v1/enterprise-customer/`;
+
+  try {
+    const { data } = await httpClient.get(enterpriseApiUrl);
+    const enterpriseCustomers = data.results.map(customer => ({
+      name: customer.name,
+      isEnabled: customer.enable_learner_portal,
+      hostname: customer.learner_portal_hostname,
+    }));
+
+    const links = enterpriseCustomers
+      .filter(({ isEnabled, hostname }) => isEnabled && hostname)
+      .map(({ name, hostname }) => ({
+        title: name,
+        url: `https://${hostname}`,
+      }));
+
+    cacheLinks(userId, links);
+
+    return links;
+  } catch (e) {
+    return [];
+  }
+};
+
+const getCachedLearnerPortalLinks = (userId) => {
+  const cacheKey = getCacheKey(userId);
+  const cachedValue = sessionStorage.getItem(cacheKey);
+
+  if (cachedValue) {
+    const cachedLinks = JSON.parse(cachedValue);
     // Check cache expiration
     if (cachedLinks.expiration <= Date.now()) {
       sessionStorage.removeItem(cacheKey);
@@ -73,23 +58,21 @@ function getCachedLearnerPortalLinks(userId) {
   }
 
   return null;
-}
+};
 
-export default async function getLearnerPortalLinks(apiClient) {
-  let learnerPortalLinks = [];
+export default async function getLearnerPortalLinks() {
   const authenticatedUser = await getAuthenticatedUser();
 
-  if (authenticatedUser !== null && isEnterpriseLearner(authenticatedUser)) {
+  if (isEnterpriseLearner(authenticatedUser)) {
     const { userId } = authenticatedUser;
     const cachedLinks = getCachedLearnerPortalLinks(userId);
 
     if (cachedLinks != null) {
-      learnerPortalLinks = learnerPortalLinks.concat(cachedLinks);
-    } else {
-      const links = await fetchLearnerPortalLinks(apiClient, userId);
-      learnerPortalLinks = learnerPortalLinks.concat(links);
+      return cachedLinks;
     }
+
+    return fetchLearnerPortalLinks(userId);
   }
 
-  return learnerPortalLinks;
+  return [];
 }
