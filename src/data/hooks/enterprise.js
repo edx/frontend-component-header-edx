3b7c815d810171863332a739793afebf01299cb5
Jeff LaJoie <jeffrey.lajoie@gmail.com>
2020-08-11 09:37:10 -0400

    fix: adds in ENT env var and updates calls to frontend-ent

--- src/data/hooks/enterprise.js
+++ src/data/hooks/enterprise.js
@@ -2,13 +2,18 @@ import { useEffect, useState } from 'react';
 import { getLearnerPortalLinks, getSelectedEnterpriseUUID } from '@edx/frontend-enterprise';
 import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 
-export default function useEnterpriseConfig(authenticatedUser) {
+export default function useEnterpriseConfig(authenticatedUser, config) {
   const [enterpriseLearnerPortalLink, setEnterpriseLearnerPortalLink] = useState();
   const [enterpriseCustomerBrandingConfig, setEnterpriseCustomerBrandingConfig] = useState();
   useEffect(
     () => {
       const httpClient = getAuthenticatedHttpClient();
-      getLearnerPortalLinks(httpClient, authenticatedUser).then((learnerPortalLinks) => {
+      getLearnerPortalLinks(
+        httpClient,
+        authenticatedUser,
+        config.ENTERPRISE_LEARNER_PORTAL_HOSTNAME,
+        config.LMS_BASE_URL,
+      ).then((learnerPortalLinks) => {
         const preferredUUID = getSelectedEnterpriseUUID(authenticatedUser);
         const preferredLearnerPortalLink = learnerPortalLinks.find(learnerPortalLink =>
           learnerPortalLink.uuid === preferredUUID);
