d65c20ed99d093053c7c54ccb6c77f90e6c48aab
Gábor Boros <gabor.brs@gmail.com>
2020-10-09 18:25:13 +0200

    feat: add new studio header variant

---
+++ src/StudioHeader.jsx
@@ -0,0 +1,77 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
+import { AppContext } from '@edx/frontend-platform/react';
+
+import Header from './Header';
+import { menuItemShape, itemDetailsShape } from './data/shapes/header';
+import messages from './StudioHeader.messages';
+
+export class StudioHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  renderItemDetails() {
+    const { itemDetails } = this.props;
+
+    return (
+      <>
+        <nav className="nav item-details-container">
+          <div className="nav-link item-details">
+            <a href={itemDetails.url} className="item-link">
+              <span className="item-detail item-org">{itemDetails.org}</span>
+              <span className="item-detail item-id">{itemDetails.id}</span>
+              <span className="item-detail item-title" title={itemDetails.title}>{itemDetails.title}</span>
+            </a>
+          </div>
+        </nav>
+      </>
+    );
+  }
+
+  render() {
+    const {
+      intl, mainMenu, extraMainMenuItems, itemDetails,
+    } = this.props;
+    const { config } = this.context;
+
+    const menu = mainMenu || [
+      {
+        type: 'item',
+        href: `${config.STUDIO_BASE_URL}/home/`,
+        content: intl.formatMessage(messages['header.links.courses']),
+      },
+      {
+        type: 'item',
+        href: `${config.LIB_AUTHORING_BASE_URL}/`,
+        content: intl.formatMessage(messages['header.links.libraries']),
+      },
+      ...extraMainMenuItems,
+    ];
+
+    return (
+      <Header variant="studio" mainMenu={menu}>
+        {itemDetails && this.renderItemDetails()}
+      </Header>
+    );
+  }
+}
+
+StudioHeader.contextType = AppContext;
+
+StudioHeader.propTypes = {
+  intl: intlShape.isRequired,
+  itemDetails: itemDetailsShape,
+  mainMenu: PropTypes.arrayOf(menuItemShape),
+  extraMainMenuItems: PropTypes.arrayOf(menuItemShape),
+};
+
+StudioHeader.defaultProps = {
+  itemDetails: null,
+  mainMenu: null,
+  extraMainMenuItems: [],
+};
+
+export default injectIntl(StudioHeader);
--- src/data/shapes/header.js
+++ src/data/shapes/header.js
@@ -4,5 +4,13 @@ export const menuItemShape = PropTypes.shape({
   type: PropTypes.string.isRequired,
   href: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
+  submenuContent: PropTypes.string,
   onClick: PropTypes.func,
+});
+
+export const itemDetailsShape = PropTypes.shape({
+  id: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  org: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 });
