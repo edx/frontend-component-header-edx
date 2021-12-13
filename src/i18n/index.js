66fcc0c9a38474e01a604ad1d320598a676e75db
Asad Iqbal <7334669+asadiqbal08@users.noreply.github.com>
2021-12-13 19:11:48 +0500

    feat: introduced LearningHeader for frontend-app-learning (#196)
    * feat: introduced LearningHeader for frontend-app-learning
    
    * refactor: Addressed feedback

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,5 +1,14 @@
 import arMessages from './messages/ar.json';
 // no need to import en messages-- they are in the defaultMessage field
+
+import caMessages from './messages/ca.json';
+import heMessages from './messages/he.json';
+import idMessages from './messages/id.json';
+import plMessages from './messages/pl.json';
+import ruMessages from './messages/ru.json';
+import thMessages from './messages/th.json';
+import ukMessages from './messages/uk.json';
+
 import es419Messages from './messages/es_419.json';
 import frMessages from './messages/fr.json';
 import kokrMessages from './messages/ko_KR.json';
@@ -8,6 +17,13 @@ import zhcnMessages from './messages/zh_CN.json';
 
 const messages = {
   ar: arMessages,
+  ca: caMessages,
+  he: heMessages,
+  id: idMessages,
+  pl: plMessages,
+  ru: ruMessages,
+  th: thMessages,
+  uk: ukMessages,
   'es-419': es419Messages,
   fr: frMessages,
   'zh-cn': zhcnMessages,
