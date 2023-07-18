b1439554d9ac484d27c79324a9bea7dec69e5bac
Awais Ansari <79941147+awais-ansari@users.noreply.github.com>
2023-07-18 14:11:42 +0500

    feat: added notifications UI (#407)
    * feat: added notifications UI
    
    * chore: generated package-lock file using NPM 9
    
    * feat: link setting icon with notification preferences page

--- src/learning-header/LearningHeader.jsx
+++ src/learning-header/LearningHeader.jsx
@@ -1,13 +1,26 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { useEnterpriseConfig } from '@edx/frontend-enterprise-utils';
-import { getConfig } from '@edx/frontend-platform';
+import {
+  APP_CONFIG_INITIALIZED, getConfig, ensureConfig, subscribe, mergeConfig,
+} from '@edx/frontend-platform';
 import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
-import { AppContext } from '@edx/frontend-platform/react';
+import { AppContext, AppProvider } from '@edx/frontend-platform/react';
 
 import AnonymousUserMenu from './AnonymousUserMenu';
 import AuthenticatedUserDropdown from './AuthenticatedUserDropdown';
 import messages from './messages';
+import store from '../store';
+
+ensureConfig([
+  'ACCOUNT_SETTINGS_URL',
+], 'Learning Header component');
+
+subscribe(APP_CONFIG_INITIALIZED, () => {
+  mergeConfig({
+    ACCOUNT_SETTINGS_URL: process.env.ACCOUNT_SETTINGS_URL || '',
+  }, 'Learning Header additional config');
+});
 
 const LinkedLogo = ({
   href,
@@ -57,25 +70,27 @@ const LearningHeader = ({
   }
 
   return (
-    <header className="learning-header">
-      <a className="sr-only sr-only-focusable" href="#main-content">{intl.formatMessage(messages.skipNavLink)}</a>
-      <div className="container-xl py-2 d-flex align-items-center">
-        {headerLogo}
-        <div className="flex-grow-1 course-title-lockup" style={{ lineHeight: 1 }}>
-          <span className="d-block small m-0">{courseOrg} {courseNumber}</span>
-          <span className="d-block m-0 font-weight-bold course-title">{courseTitle}</span>
+    <AppProvider store={store}>
+      <header className="learning-header">
+        <a className="sr-only sr-only-focusable" href="#main-content">{intl.formatMessage(messages.skipNavLink)}</a>
+        <div className="container-xl py-2 d-flex align-items-center">
+          {headerLogo}
+          <div className="flex-grow-1 course-title-lockup" style={{ lineHeight: 1 }}>
+            <span className="d-block small m-0">{courseOrg} {courseNumber}</span>
+            <span className="d-block m-0 font-weight-bold course-title">{courseTitle}</span>
+          </div>
+          {showUserDropdown && authenticatedUser && (
+            <AuthenticatedUserDropdown
+              enterpriseLearnerPortalLink={enterpriseLearnerPortalLink}
+              username={authenticatedUser.username}
+            />
+          )}
+          {showUserDropdown && !authenticatedUser && (
+            <AnonymousUserMenu />
+          )}
         </div>
-        {showUserDropdown && authenticatedUser && (
-          <AuthenticatedUserDropdown
-            enterpriseLearnerPortalLink={enterpriseLearnerPortalLink}
-            username={authenticatedUser.username}
-          />
-        )}
-        {showUserDropdown && !authenticatedUser && (
-          <AnonymousUserMenu />
-        )}
-      </div>
-    </header>
+      </header>
+    </AppProvider>
   );
 };
 
